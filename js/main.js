1c4783eaf45fd4d33d14cca1e7141b2e9387d9b6
patriciabrsk <patriciabresku@gmail.com>
2022-02-24 20:49:17 +0100

    add comments

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-// Scrivi un programma che stampi i numeri da 1 a 100, 
+// JavaScript program to print Fizz Buzz
 
-// ma per i multipli di 3 stampi “Fizz” al posto del numero 
-// e per i multipli di 5 stampi Buzz.
-
+// Selecting element by id 
 const divBoard = document.getElementById('board');
 
+// Iterating through numbers from 0 to 100
 for (i = 0; i <= 100; i++) {
     let newDiv = document.createElement('div');
     newDiv.classList.add(
@@ -19,19 +18,22 @@ for (i = 0; i <= 100; i++) {
         );
     divBoard.append(newDiv);
 
-    // Per i numeri che sono sia multipli di 3 che di 5 stampa FizzBuzz
+    // Number divisible by 3 and 5 will print FizzBuzz, replacing existing number
     if ((i % 3 == 0) && (i % 5 == 0)) {
         newDiv.innerHTML = '<h5 class="text-primary fs-bold">FizzBuzz</h5>';
         newDiv.classList.add('shake');
 
-    // per i multipli di 3 stampa “Fizz” al posto del numero
+    // Number divisible by 3 will print Fizz, replacing existing number
     } else if (i % 3 == 0) {
         newDiv.innerHTML = '<h5 class="fs-bold my-text-neon">Fizz</h5>';
         // newDiv.classList.add('');
-    // e per i multipli di 5 stampa Buzz
+
+    // Number divisible by 5 will print Buzz, replacing existing number
     } else if (i % 5 == 0) {
         newDiv.innerHTML = '<h5 class="fs-bold">Buzz</h5>';
         newDiv.classList.add('text-dark');
+
+    // If none of the above conditions are satisfied, it just prints the number
     } else {
         newDiv.innerHTML = '<h5 class="fs-2 my-text-lightg">' + i + '</h5>';
         newDiv.classList.add('rounded-circle', 'blue-gradient', 'spin');
